238073d2f0b1a6ba5805cfd8ec0b4f4a63fb9f48
FluffySneakyFox <sergey_belayev@inbox.ru>
2022-04-25 20:50:10 +0300

    Исправил половину проекта, наверное

--- intg/Intrrr/Interface1.cs
+++ intg/Intrrr/Interface1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace intg.Intrrr
+namespace Integral.Intrrr
 {
     interface ICalcult
     {
--- intg/Intrrr/Trapec.cs
+++ intg/Intrrr/Trapec.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace intg.Intrrr
+namespace Integral.Intrrr
 {
     class Trapec : ICalcult
     {
         double ICalcult.Calcul(int count, double upLim, double downLim, Func<double, double> integral, out double time)
         {
-            double h = (upLim - downLim) / count;
+            double h = (upLim - downLim) / (double)count;
             double sum = 0;
 
-            //a += h * k;
-
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             for (int i = 1; i < count-1; i++)
             {
                 sum += integral(downLim + h * i);
             }
-            sum += integral(downLim) + integral(upLim) / 2;
-            time = 0;
+            sum += (integral(downLim) + integral(upLim)) / 2.0;
+            sw.Stop();
+
+            TimeSpan ts = sw.Elapsed;
+            time = ts.TotalMilliseconds;
             return h * sum;
         }
     }
